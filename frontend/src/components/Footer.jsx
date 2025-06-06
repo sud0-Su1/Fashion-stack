149402166801a385be2bdf85a66259d81690ca20
Abhik Saran <110584674+AbhikDevX@users.noreply.github.com>
2025-06-06 21:13:56 +0530

    Initial commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div>
             <img src={assets.logo} className='mb-5 w-32' alt="" />
             <p className='w-full md:w-2/3 text-gray-600'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              Delivery on time with us
             </p>
         </div>
 
@@ -26,7 +26,7 @@ const Footer = () => {
         <div>
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>+1-212-456-7890</li>
+                <li>+91 956783222</li>
                 <li>contact@foreveryou.com</li>
             </ul>
         </div>
@@ -35,7 +35,7 @@ const Footer = () => {
 
         <div>
             <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved by Abhik.</p>
         </div>
 
     </div>
